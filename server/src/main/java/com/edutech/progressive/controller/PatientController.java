d148318b1fcadb8f365e0fc56282ff63e092f2ea
jayavarshini164 <dummy@gmail.com>
2026-01-20 18:09:29 +0000

    solve initial commit

--- server/src/main/java/com/edutech/progressive/controller/PatientController.java
+++ server/src/main/java/com/edutech/progressive/controller/PatientController.java
@@ -1,41 +1,126 @@
 package com.edutech.progressive.controller;
-
+ 
 import com.edutech.progressive.entity.Patient;
+
+import com.edutech.progressive.service.impl.PatientServiceImplArraylist;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.PutMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+ 
 import java.util.List;
+
+@RestController
+
+@RequestMapping("/patient")
 
 public class PatientController {
 
+    @Autowired
+
+    PatientServiceImplArraylist patientServiceImplArraylist;
+ 
+    @GetMapping
+
     public ResponseEntity<List<Patient>> getAllPatients() {
-        return null;
-    }
 
-    public ResponseEntity<Patient> getPatientById(int patientId) {
         return null;
-    }
 
-    public ResponseEntity<Integer> addPatient(Patient patient) {
-        return null;
     }
+ 
+    @GetMapping("/{patientId}")
 
-    public ResponseEntity<Void> updatePatient(int patientId, Patient patient) {
-        return null;
-    }
+    public ResponseEntity<Patient> getPatientById(@PathVariable int patientId) {
 
-    public ResponseEntity<Void> deletePatient(int patientId) {
         return null;
+
     }
+ 
+    @PostMapping
+
+    public ResponseEntity<Integer> addPatient(@RequestBody Patient patient) {
+
+        return null;
+
+    }
+ 
+    @PutMapping("/{patientId}")
+
+    public ResponseEntity<Void> updatePatient(@PathVariable int patientId,@RequestBody Patient patient) {
+ 
+        return null;
+
+    }
+ 
+    @DeleteMapping("/{patientId}")
+
+    public ResponseEntity<Void> deletePatient(@PathVariable int patientId) {
+
+        return null;
+
+    }
+ 
+    @GetMapping("/fromArrayList")
 
     public ResponseEntity<List<Patient>> getAllPatientFromArrayList() {
-        return null;
-    }
 
-    public ResponseEntity<Void> addPatientToArrayList() {
-        return null;
+        return new ResponseEntity<List<Patient>>(patientServiceImplArraylist.getAllPatients(), HttpStatus.OK);
+
     }
+ 
+    @PostMapping("/toArrayList")
+
+    public ResponseEntity<Void> addPatientToArrayList(Patient patient) {
+
+        try {
+
+            patientServiceImplArraylist.addPatient(patient);
+
+            return new ResponseEntity<>(HttpStatus.CREATED);
+
+        }
+
+         catch (Exception e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+    }
+ 
+    @GetMapping("/fromArrayList/sorted")
 
     public ResponseEntity<List<Patient>> getAllPatientSortedByNameFromArrayList() {
-        return null;
+
+        try {
+
+            return new ResponseEntity<List<Patient>>(patientServiceImplArraylist.getAllPatientSortedByName(), HttpStatus.OK);
+
+        } catch (Exception e) {
+
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
     }
+
 }
+ 
--- server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplArraylist.java
@@ -1,44 +1,68 @@
 package com.edutech.progressive.service.impl;
-
+ 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
+import java.util.Collections;
+
+import java.util.Comparator;
+
+import java.util.List;
+ 
 import com.edutech.progressive.entity.Patient;
+
 import com.edutech.progressive.service.PatientService;
 
+// @Service
+
 public class PatientServiceImplArraylist implements PatientService {
+
     private static List<Patient> patientList = new ArrayList<>();
-    
+
     @Override
+
     public List<Patient> getAllPatients() {
-        return patientList;
-    }
 
+        return patientList;
+
+    }
+ 
     @Override
+
     public Integer addPatient(Patient patient) {
+
         patientList.add(patient);
+
         return patient.getPatientId();
-    }
 
+    }
+ 
     @Override
+
     public List<Patient> getAllPatientSortedByName() {
+
         Collections.sort(patientList, NameComparator);
+
         return patientList;
-    }
 
+    }
+ 
     public static Comparator<Patient> NameComparator = new Comparator<Patient>() {
-
+ 
         @Override
+
         public int compare(Patient arg0, Patient arg1) {
+
             return arg0.getFullName().compareTo(arg1.getFullName());
+
         }
-        
+
     };
-
+ 
     public void emptyArrayList(){
-        patientList.clear();
-    }
 
+        patientList.clear();
+
+    }
+ 
 }
+ 
