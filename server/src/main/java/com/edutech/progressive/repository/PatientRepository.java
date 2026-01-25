79870757e31e0b1d5197490fab64c48ff71540d5
jayavarshini164 <dummy@gmail.com>
2026-01-25 05:18:10 +0000

    Day 9

--- server/src/main/java/com/edutech/progressive/repository/PatientRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/PatientRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.edutech.progressive.entity.Patient;
-//@Repository
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer>{
 
     Patient findByPatientId(int patientId);
+
+    Patient findByEmail(String email);
 }
